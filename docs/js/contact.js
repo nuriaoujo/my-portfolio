77029db6b93363490e0955a00e9ea00e17222e16
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-02-29 13:02:59 +0100

    new docs created with changes un js

--- docs/js/contact.js
+++ docs/js/contact.js
@@ -3,4 +3,7 @@ var submitted=false;
 document.getElementById("gform").addEventListener('submit', () => {
     const dialog = document.getElementsByClassName("dialog-style");
     dialog[0].setAttribute("popup", "open");
+    setTimeout(() => {
+        dialog[0].setAttribute("popup", "close");
+    }, 4000);
 });
