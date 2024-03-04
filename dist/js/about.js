884aba5913d7dd09373f9a6f42ed4250daf8681c
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-03-04 23:53:43 +0100

    selector customized finished

--- dist/js/about.js
+++ dist/js/about.js
@@ -17,12 +17,17 @@ document.getElementById("nextpage").onclick = function() {nextPage()};
 
 function openCVOption() {
     const cVBtn = document.getElementsByClassName("options");
+    const changeBorderRadiusCv = document.getElementsByClassName("btn-2");
     const cVChervon = document.getElementById("download-chevron");
     if ( cVBtn[0].style.opacity == "1") {
         cVBtn[0].style.opacity = "0";
+        cVBtn[0].style.visibility = "hidden";
+        changeBorderRadiusCv[0].style.borderRadius  = "9px";
         cVChervon.style.transform = "rotate(0deg)";
     } else {
         cVBtn[0].style.opacity = "1";
+        cVBtn[0].style.visibility = "visible";
+        changeBorderRadiusCv[0].style.borderRadius  = "9px 9px 0 0";
         cVChervon.style.transform = "rotate(180deg)";
     }
 }
